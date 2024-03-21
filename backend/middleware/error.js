c91fc536c5a7502175a287fb4f6d646811ee5a98
Devanshu Agarwal <agarwal.devanshu12@gmail.com>
2024-03-21 18:53:00 +0530

    mongoose duplicate key err + JWT expire err

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,18 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHander(message, 400);
   }
 
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Account already exists`;
+    err = new ErrorHander(message, 400);
+  }
+
+  // JWT expire error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token is Expired, Try again`;
+    err = new ErrorHander(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
