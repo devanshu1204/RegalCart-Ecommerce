5110f84c10544352114ff7e952dd0e7b594b31a7
Devanshu Agarwal <agarwal.devanshu12@gmail.com>
2024-03-22 15:08:03 +0530

    user profile endpoint

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -5,7 +5,11 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  userProfile,
 } = require("../controllers/usersController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
 const router = express.Router();
 
 router.route("/register").post(register);
@@ -18,4 +22,5 @@ router.route("/password/forgot").post(forgotPassword);
 
 router.route("/password/reset/:token").put(resetPassword);
 
+router.route("/me").get(isAuthenticatedUser, userProfile);
 module.exports = router;
